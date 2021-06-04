b7780cdacc0949fe1efa252f5e2b7390051842de
Haru Kaneda <haru_kaneda@icloud.com>
2021-06-04 12:04:41 -0400

    added the correct sound folder and settings

--- Sound Board/ViewController.swift
+++ Sound Board/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+      playSound(soundName: sender.currentTitle!)
+        print(sender.currentTitle)
+}
+    func playSound(soundName: String) {
+        guard let url = Bundle.main.url(forResource: soundName, withExtension: "mp3") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 
 }
 
